482bf9883f954417893150d0bfc2896b30c45ad0
EkeneOzoalor <88558745+EkeneOzoalor@users.noreply.github.com>
2022-03-16 07:03:10 -0700

    chnages

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 WHITELIST_CONTRACT_ADDRESS = "0xC6Cc63d6eC875E84F9cba7D164D7AcA162E25C5B";
-METADATA_URL = "nft-collection-g8fl49udu-frankozoalor.vercel.app/api";
+METADATA_URL = "opensa-nft-collection.vercel.app/api/";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
